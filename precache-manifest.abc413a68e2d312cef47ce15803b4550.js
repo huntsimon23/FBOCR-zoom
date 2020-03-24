b71d298ef11b830313e3e0cf16f02e74333be767
huntsimon23 <hunt.simon23@gmail.com>
2020-03-24 06:27:19 -0400

    Updates

---
+++ precache-manifest.abc413a68e2d312cef47ce15803b4550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f82c42a06ce50b7e9ea2db57f6fb19d",
+    "url": "/fbocr-zoom/index.html"
+  },
+  {
+    "revision": "df564bc2a26cb5f26d4c",
+    "url": "/fbocr-zoom/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e26b85714b6908718c01",
+    "url": "/fbocr-zoom/static/js/2.5018f732.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fbocr-zoom/static/js/2.5018f732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df564bc2a26cb5f26d4c",
+    "url": "/fbocr-zoom/static/js/main.3f3b854e.chunk.js"
+  },
+  {
+    "revision": "c7cc0d6ca25055a16a15",
+    "url": "/fbocr-zoom/static/js/runtime-main.ad94a70c.js"
+  }
+]);
