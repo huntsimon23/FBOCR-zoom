61b95a35e2827f373764782592ac3bbe8a8a8621
huntsimon23 <hunt.simon23@gmail.com>
2020-04-07 14:49:49 -0400

    Updates

--- precache-manifest.3fe18b6b7887252ae17d6415101004d5.js
+++ precache-manifest.3fe18b6b7887252ae17d6415101004d5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "138cf83022e7727dbea8dca78fa49c17",
+    "revision": "e291c07e9d4bad8a4efacf3e1f0878fd",
     "url": "/FBOCR-zoom/index.html"
   },
   {
-    "revision": "a8235194cb6d25e38c28",
+    "revision": "a5a828d055eadecfabb0",
     "url": "/FBOCR-zoom/static/css/main.b34d45c7.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/FBOCR-zoom/static/js/2.0e8907c2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a8235194cb6d25e38c28",
-    "url": "/FBOCR-zoom/static/js/main.6cffd48c.chunk.js"
+    "revision": "a5a828d055eadecfabb0",
+    "url": "/FBOCR-zoom/static/js/main.ca4586c9.chunk.js"
   },
   {
     "revision": "c25e7cba3c6ea518577f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FBOCR-zoom/precache-manifest.73d5e5714df508e41e11e20e5aa9b4f4.js"
+  "/FBOCR-zoom/precache-manifest.3fe18b6b7887252ae17d6415101004d5.js"
 );
 
 self.addEventListener('message', (event) => {
