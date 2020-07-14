2a662b80f75174eadc8eb77c84dfd2f3b171b3eb
huntsimon23 <hunt.simon23@gmail.com>
2020-07-14 12:58:10 -0400

    link correction

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -5,7 +5,7 @@ class Youtube extends Component {
 render(){
 return (
 <div className="iframe"> 
-<iframe width="auto" height="auto" src="https://youtu.be/9ORqiHw3olA?t=712" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="auto" height="auto" src="https://www.youtube.com/embed/9ORqiHw3olA?t=712" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 )}
 }
