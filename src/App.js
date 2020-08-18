05a331740bdbf4fbe605f1aaf78b0e956bd10730
huntsimon23 <hunt.simon23@gmail.com>
2020-08-18 16:30:25 -0400

    redirect

--- src/App.js
+++ src/App.js
@@ -14,64 +14,10 @@ class App extends Component {
   render() {
   return (
     <div className="App container container-fluid">
-      <header className="App-header">
       <Header />
-      </header>
-      <hr />
-      {/* <Youtube2 /> */}
-      <Button 
-       id="Announcements"
-       key="Announcements"
-       name="Weekly Announcements and more at FirstOrlandoRecovery.com"
-       link="http://firstorlandorecovery.com"
-      />
-      <h2 className="text-center">
-      <span>This week's video: 
-      Lesson 05: Turn</span>   
-      </h2>
-      <br />   
-      <Youtube />
-      <hr />
-      <div className = "row">
-      <div className = "col">
-      <h2>Open Share Groups - Start at 7:30pm EST</h2>
-      <h6 className="text-center">
-      <span>These rooms will open at 7:30pm on Tuesday nights beginning 3/25/2020.  The group host will go over guidelines that must be followed at ALL times.  The group host has been asked to strictly enforce these guidelines for the safety of the entire group.  You can read more about these guidelines at <a href="https://www.celebraterecovery.com/crcr" font-weight="strong" target="_blank">CelebrateRecovery.com</a>
-      <br />
-      <br />
-      A password is required to access the room - if you do not have one, contact <a href="mailto:celebraterecovery@firstorlando.com">celebraterecovery@firstorlando.com</a>.  Please note that all participants are required to complete Newcomers 101 orientation before joining an online FBOCR open share group!  </span>   
-      </h6>
-      </div>
-      </div>
-      <hr />
-        <div className="container container-fluid">
-          {zoomdata.map(zoom => (
-          <div key={zoom.id}>  
-          <Button  
-          id={zoom.id}
-          key={zoom.id}
-          name={zoom.name}
-          link={zoom.link}
-          />
-          </div>
-          ))}  
-        </div>
-      <div>
-        <h4>New to Zoom?  Looking for some tech pointers?</h4>
-        <a href="https://www.celebraterecovery.com/crcr/95-crcr/224-zoom-videos" target = "_blank">
-        <button className="btn btn-danger btn-lg">
-        An overview of Zoom & CR
-        </button></a>
-        <br />
-        <Button 
-          id="10"
-          key="10"
-          name="Zoom Ettiquette"
-          link="https://drive.google.com/file/d/1fdw79xTlMFCsjoF8AhA18lQ6N4ziFFn0/view"
-        />
+      
         <br />
         <br />
-      </div>
     </div>
 
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -14,13 +14,14 @@ return (
     <br />
     <h1 className="text-center">First Baptist Orlando's Online Open Share Groups</h1>
     <br />
-    <h4>Please note: These groups are open for all who have been thru our Newcomers 101 orentation here at FBOCR.  If you are new to CR at First Orlando, please complete this short form and a member of our team will schedule a Newcomers 101 session with you! </h4>
+    <h4>ALL FBO CR Online Groups Have Moved to the new website!  
+    Click button below to be redirected.</h4>
     <br />
     <Button 
-       id="Newcomers 101"
-       key="Newcomers 101"
-       name="Request Newcomers 101 Orientation"
-       link="https://fbocr.churchcenter.com/people/forms/117331"
+       id="New Site"
+       key="New Site"
+       name="visit firstorlandorecovery.com"
+       link="https://firstorlandorecovery.com"
     />
 </div>
 )}
